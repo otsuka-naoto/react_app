c8a555bc26e3f56c8ad5f0acd3f7cea57e87a79d
otsuka-naoto <naoto1891@gmail.com>
2020-12-01 22:53:23 +0900

    コンポーネント切り出し

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import React from './React';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  msgStyle = {
+    fontSize: "24px",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solod#900"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'Hello Component.',
+    };
+
+  }
+
+  render() {
+    return <div><h1>React</h1>
+      <p>{this.state.msg}</p>
+      <p>{this.props.msg}</p></div>
+  }
+
 }
+
 
 export default App;
